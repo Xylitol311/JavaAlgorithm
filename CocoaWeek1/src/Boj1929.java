c03d334be32d3e90f0a372e5aee4fce93f05717d
ramu311 <cktnghd0311@daum.net>
2021-11-08 15:10:41 +0900

    Modify Boj1929.java

--- CocoaWeek1/src/Boj1929.java
+++ CocoaWeek1/src/Boj1929.java
@@ -1,27 +1,34 @@
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Boj1929 {
-    public  static   void main(String[] args) {
-        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in);
+    public static boolean[] prime;
+    public static void main(String[] args) {
 
-        StringTokenizer sto = new StringTokenizer((buf.readLine(), " "));
+        Scanner sc = new Scanner(System.in);
+        int min = sc.nextInt();
+        int max = sc.nextInt();
 
-        int min = Integer.parseInt(sto.nextToken());
-        int max = Integer.parseInt(sto.nextToken());
+        prime = new boolean[max + 1];
+        get_prime();
 
-        boolean[] arr = new boolean[max + 1];
-        arr[0] = true;
-        arr[1] = true;
+        for(int i = min; i <= max; i++) {
+            //소수는 false. false값 출력
+            if(!prime[i]) System.out.println(i);
+        }
+    }
 
-        for (int i = 2; i <= Math.sqrt(max); i++) {
-            if (arr[i] == true) continue;
-            for (int j = i * i; j < arr.length; j+=i) {
+    public static void get_prime() {
+        prime[0] = prime[1] = true; //0과 1은 소수가 아니니까 true
 
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue; //prime[i]가 true면 이미 그 배수는 true로 변환되었기 때문에 건너뜀
+            for(int j = i * i; j < prime.length; j += i) {//소수일 경우 실행. 소수값의 제곱값부터 소수를 더해가며 소수의 배수를 true로 변환
+                prime[j] = true;
             }
-
         }
     }
 }
