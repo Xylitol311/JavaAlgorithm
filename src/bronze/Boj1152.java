fc21bf779c10c829ee2dce9b0e02efcbda4e6187
Xylitol311 <soohong0311@gmail.com>
2022-12-30 03:23:14 +0900

    Modify Boj1152.java
    1152번 문제 해결. 속도 개선을 위해 BufferedReader 사용해 볼 필요 있음.

--- src/bronze/Boj1152.java
+++ src/bronze/Boj1152.java
@@ -1,4 +1,18 @@
 package bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Boj1152 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String S = in.nextLine();
+        in.close();
+        StringTokenizer st = new StringTokenizer(S, " ");
+        System.out.println(st.countTokens());
+    }
 }
