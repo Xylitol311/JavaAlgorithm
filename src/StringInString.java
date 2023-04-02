4262d6a21a1c3c6c94cdc1b6d3ff00b6e631012f
Xylitol311 <soohong0311@gmail.com>
2023-04-02 12:11:55 +0900

    프로그래머스 문자열 안에 문자열 문제 풀이

---
+++ src/StringInString.java
@@ -0,0 +1,16 @@
+
+public class StringInString {
+    public static void main(String[] args) {
+        StringInString sol = new StringInString();
+        String str1 = "ab6CDE443fgh22iJKlmn1o";
+        String str2 = "6CD";
+        System.out.println("정답은 " + sol.solution(str1, str2));
+
+    }
+    public int solution(String str1, String str2) {
+        if (str1.contains(str2)) {
+            return 1;
+        }
+        return 2;
+    }
+}
