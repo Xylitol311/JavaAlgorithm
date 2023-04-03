b026fb83124fefb851af8475f743e6e70b85a35a
Xylitol311 <soohong0311@gmail.com>
2023-04-03 11:31:31 +0900

    백준 2557번 문제 풀이

---
+++ src/baekjoon/bronze/Boj2557.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
