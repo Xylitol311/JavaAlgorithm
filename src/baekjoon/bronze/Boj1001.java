5d2a8690b6bf6d5c3a1ac6b1f9230df9c103a426
Xylitol311 <soohong0311@gmail.com>
2023-04-06 13:30:01 +0900

    백준 1001번 문제 풀이

---
+++ src/baekjoon/bronze/Boj1001.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Boj1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a-b);
+    }
+}
