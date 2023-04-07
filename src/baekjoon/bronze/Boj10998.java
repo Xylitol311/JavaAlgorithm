436c168a1d112ec25ae0697efdabe36d7437e2a0
Xylitol311 <soohong0311@gmail.com>
2023-04-07 15:30:01 +0900

    백준 10998번 문제 풀이

---
+++ src/baekjoon/bronze/Boj10998.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Boj10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a*b);
+    }
+}
