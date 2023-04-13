3830ac97a3d1f39aa47870fce6b6801875b68d2e
Xylitol311 <soohong0311@gmail.com>
2023-04-13 18:11:07 +0900

    백준 18108번  문제 풀이

---
+++ src/baekjoon/bronze/Boj18108.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Boj18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println(a - 543);
+    }
+}
