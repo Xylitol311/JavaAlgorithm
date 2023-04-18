b85f14fc95373ee749bb4669124e27d1db3eec8a
Xylitol311 <soohong0311@gmail.com>
2023-04-18 14:14:08 +0900

    프로그래머스 두 수의 합 풀이

---
+++ src/programmers/PlusTwoNumbers.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class PlusTwoNumbers {
+    public static void main(String[] args) {
+        PlusTwoNumbers sol = new PlusTwoNumbers();
+        String str1 = "ab6CDE443fgh22iJKlmn1o";
+        String str2 = "6CD";
+        System.out.println("정답은 " + sol.solution(2, 3));
+
+    }
+    public int solution(int num1, int num2) {
+        int answer = -1;
+
+        answer = num1 + num2;
+        return answer;
+    }
+}
