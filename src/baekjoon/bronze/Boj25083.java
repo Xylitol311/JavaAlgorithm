a8915b01f2cd7dee96e92c82b97ab9345b14b67f
Xylitol311 <soohong0311@gmail.com>
2023-04-21 10:34:16 +0900

    백준 25083번 문제 풀이

---
+++ src/baekjoon/bronze/Boj25083.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze;
+
+public class Boj25083 {
+    public static void main(String[] args) {
+        String answer =
+                "         ,r'\"7\n" +
+                        "r`-_   ,'  ,/\n" +
+                        " \\. \". L_r'\n" +
+                        "   `~\\/\n" +
+                        "      |\n" +
+                        "      |";
+        System.out.println(answer);
+    }
+}
