8a530593500f91c2560715791199baa4fcc11936
Xylitol311 <soohong0311@gmail.com>
2023-04-26 18:41:02 +0900

    백준 14681번 문제 풀이

---
+++ src/baekjoon/bronze/Boj14681.java
@@ -0,0 +1,24 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Boj14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int answer = 0;
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x > 0 && y > 0) {
+            answer = 1;
+        } else if (x < 0 && y > 0) {
+            answer = 2;
+        } else if (x < 0 && y < 0) {
+            answer = 3;
+        } else {
+            answer = 4;
+        }
+        System.out.println(answer);
+    }
+}
