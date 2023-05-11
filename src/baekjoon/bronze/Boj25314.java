2b6a29e7d64efbb379bfb50f0ee747ff9d3f76e3
Xylitol311 <soohong0311@gmail.com>
2023-05-11 11:47:18 +0900

    백준 25314번 문제 풀이

---
+++ src/baekjoon/bronze/Boj25314.java
@@ -0,0 +1,21 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj25314 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String str = "";
+        br.close();
+
+        for(int i=1; i<=n/4; i++) {
+            str += "long ";
+        }
+        System.out.println(str + "int");
+    }
+}
