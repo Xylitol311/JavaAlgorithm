20e31cedf4bd79a70180bb2353d362caf3b0989d
Xylitol311 <soohong0311@gmail.com>
2024-02-23 17:08:31 +0900

    프로그래머스 문제 풀이

---
+++ src/programmers/IHateSameNum.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+public class IHateSameNum {
+    public static void main(String[] args) {
+
+    }
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        List<Integer> array = new ArrayList<>();
+        int value = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != value){
+                array.add(arr[i]);
+                value = arr[i];
+            }
+        }
+
+        answer = array.stream().mapToInt(i -> i).toArray();
+
+        return answer;
+    }
+}
---
+++ src/programmers/PhoneNumList.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+public class PhoneNumList {
+    public static void main(String[] args) {
+
+    }
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String s : phone_book){
+            map.put(s, 1);
+        }
+
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 0; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0, j))){
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
+}
