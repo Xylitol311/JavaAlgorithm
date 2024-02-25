8dd7e3cc0167527636fc527c09120908c0f7ccdc
Xylitol311 <soohong0311@gmail.com>
2024-02-25 15:00:38 +0900

    백준 11659 문제 풀이

---
+++ src/baekjoon/Boj11659.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj11659{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        long[] nums = new long[N + 1];
+        st = new StringTokenizer(bf.readLine());
+
+        for(int i = 1; i <= N; i++){
+            nums[i] = nums[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(bf.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(nums[end] - nums[start - 1]);
+        }
+    }
+}
