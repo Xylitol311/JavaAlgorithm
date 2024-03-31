032ca0ac43c41e0b0481aa2c29ded3a5f0e99f34
Xylitol311 <soohong0311@gmail.com>
2024-03-31 15:13:43 +0900

    백준 28278번 스택 2 문제 풀이

--- src/baekjoon/silver/Boj28278.java
+++ src/baekjoon/silver/Boj28278.java
@@ -1,32 +1,38 @@
-package baekjoon;
+package baekjoon.silver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class Boj1094 {
-
+public class Boj28278 {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
-        int[] nums = new int[]{64, 32, 16, 8, 4, 2, 1};
-        int count = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
-        for (int i : nums) {
-            if (X == i) {
-                System.out.println(1);
-                return;
+        for (int i = 0; i < N; i++) {
+            String query = br.readLine();
+
+            switch (query.charAt(0)) {
+                case '1' :
+                    stack.push(Integer.parseInt(query.substring(2)));
+                    break;
+                case '2' :
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                    break;
+                case '3' :
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case '4' :
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case '5' :
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                    break;
             }
         }
-
-        for (int i = 1; i < nums.length; i++) {
-            if (X == nums[i]) {
-                System.out.println(++count);
-                return;
-            }
-            if (X > nums[i]) {
-                count++;
-                X -= nums[i];
-            }
-        }
+        System.out.println(sb);
     }
 }
