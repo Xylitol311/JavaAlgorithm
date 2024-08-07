59c7e29f1b169d97cec30f278f8e77b87c399f13
Soohong Cha <77662632+Xylitol311@users.noreply.github.com>
2024-08-07 16:51:42 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println(a-b);
-    }
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int A = sc.nextInt();
+	int B =sc.nextInt();
+	
+	if ((A>0)&&(B<10)) {
+		System.out.println(A-B);
+	}else {
+		System.out.println("범위를 벗어났습니다.");
+	}
+}
 }
