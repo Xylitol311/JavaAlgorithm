5bd8cce37ef512851e8ec1b47a3094368af74eea
Soohong Cha <77662632+Xylitol311@users.noreply.github.com>
2024-08-07 16:55:50 +0900

    [Bronze V] Title: 검증수, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int sum = 0;
-		for (int i = 0; i < 5; i++) {
-			sum += Math.pow(sc.nextInt(), 2);
+		int nums =0;
+		for (int i=0; i<5; i++) {
+			int num = sc.nextInt();
+			nums +=num*num;
 		}
-		System.out.println(sum % 10);
+		
+		int remainder = (nums%10);
+		System.out.println(remainder);
+		
 	}
 }
