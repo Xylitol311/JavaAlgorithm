c3d9f0d88f2b9adefa6d5e9d87844037b064577c
Soohong Cha <77662632+Xylitol311@users.noreply.github.com>
2024-08-07 17:18:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		char a = sc.next().charAt(0);
-		System.out.println((int) a);
+		
+		String c= sc.nextLine();
+		
+		char asci= c.charAt(0);
+		int ans = (int)asci;
+		
+		System.out.println(ans);
+
+
 	}
+
 }
