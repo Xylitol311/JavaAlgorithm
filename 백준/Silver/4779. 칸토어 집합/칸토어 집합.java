4cbd77e61e0423805f2762f364d2ca3e28be8368
Soohong Cha <77662632+Xylitol311@users.noreply.github.com>
2024-08-26 21:19:06 +0900

    [Silver III] Title: 칸토어 집합, Time: 184 ms, Memory: 21844 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -3,37 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static StringBuilder answer;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input;
-        StringBuilder result = new StringBuilder();
-
-        while ((input = br.readLine()) != null && !input.isEmpty()) {
-            int N = Integer.parseInt(input);
-            int len = (int) Math.pow(3, N);
-            char[] arr = new char[len];
+        String str;
+        while ((str = br.readLine()) != null) {
+            n = Integer.parseInt(str);
+            answer = new StringBuilder();
+            int len = (int) Math.pow(3, n);
+            //길이가 3^n 이고 '-'로 이루어진 문자열 미리 만들어 놓고, 재귀함수 시작하기
             for (int i = 0; i < len; i++) {
-                arr[i] = '-';
+                answer.append("-");
             }
-            
-            generateCantorSet(arr, len);
 
-            result.append(arr).append("\n");
+            recursion(0, len); // 시작 인덱스, 시작 길이
+            System.out.println(answer);
+
         }
-
-        System.out.print(result.toString());
     }
 
-    public static void generateCantorSet(char[] arr, int length) {
-        int segment = length;
-        while (segment > 1) {
-            int third = segment / 3;
-            for (int i = 0; i < length; i += segment) {
-                for (int j = i + third; j < i + 2 * third; j++) {
-                    arr[j] = ' ';
-                }
-            }
-            segment /= 3;
+    public static void recursion(int start, int size) { //시작 인덱스, 문자열 사이즈
+        //size가 1인 경우 리턴
+        if (size == 1) {
+            return;
         }
+        int newSize = size / 3;
+
+        // 3등분한 문자열의 가운데는 공백 처리
+        for (int i = start + newSize; i < start + 2 * newSize; i++) {
+            answer.setCharAt(i, ' ');
+        }
+
+        recursion(start, newSize); // 3등분한 문자열에서 첫번째 부분
+        recursion(start + 2 * newSize, newSize); // 3등분한 문자열의 세번째 부분
     }
 }
