d1a9611d52516a70a31d748d6a55bbbdc787306c
Soohong Cha <77662632+Xylitol311@users.noreply.github.com>
2024-08-28 03:07:35 +0900

    [Silver IV] Title: Project Teams, Time: 244 ms, Memory: 27728 KB -BaekjoonHub

---
+++ 백준/Silver/20044. Project Teams/Project Teams.java
@@ -0,0 +1,26 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] students = new int[2 * N];
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = sc.nextInt();
+        }
+
+        Arrays.sort(students);
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int tmp = students[i] + students[students.length - 1 - i];
+            answer = Math.min(answer, tmp);
+        }
+        System.out.println(answer);
+    }
+}
