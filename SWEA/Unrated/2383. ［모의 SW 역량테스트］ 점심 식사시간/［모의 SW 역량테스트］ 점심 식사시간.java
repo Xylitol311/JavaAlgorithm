f3e1b61742a29c1fc9b5b5d25cfc5cb20d027de8
Soohong Cha <77662632+Xylitol311@users.noreply.github.com>
2024-08-30 15:10:47 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 212 ms, Memory: 30,992 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -7,6 +7,22 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 테스트 케이스 개수 입력
+ * 2. 테스트 케이스 별로
+ * 	2-1. 계단의 최소 시간 초기화(정답 변수)
+ * 	2-2. 맵 크기 및 정보 입력
+ * 
+ * 3. 사람마다 갈 수 있는 계단을 재귀로 선택
+ * 	3-1. 모든 사람이 계단을 선택한 경우
+ * 	3-2. 사람들의 상태 초기화(이전 계단 조합에서 사용한 정보를 초기화)
+ * 	3-3. 모든 사람이 계단을 내려간 최소 시간 갱신
+ * 
+ * 4. 선택한 계단으로 이동
+ * 	4-1. 사람들이 남아있는 경우
+ * 		4-1-1. 계단에 내려갈 수 있는 사람이 있으면 추가
+ * 		4-1-2. 도착한 사람이 있으면 도착 처리 및 추가로 진입 가능한 사람 추가
+ */
 public class Solution {
 	static BufferedReader br;
 	static StringBuilder sb;
@@ -55,6 +71,7 @@ public class Solution {
 		}
 	}
 	
+	// 데이터 입력
 	static int minDownStairTime;
 	static int mapSize;
 	static int[][] map;
@@ -85,6 +102,7 @@ public class Solution {
 		}
 	}
 	
+	// 모든 사람이 계단을 선택한 경우 최소 시간을 구하기 전 사람들의 정보 초기화
 	public static void initializedStatus() {
 		for (int peopleIndex = 0; peopleIndex < peopleList.size(); peopleIndex++) {
 			People people = peopleList.get(peopleIndex);
@@ -93,14 +111,18 @@ public class Solution {
 			
 			Stair stair = stairList.get(selectedStairList[peopleIndex]);
 			
+			// 계단까지 도착하는 시간
 			people.arrivedTime = Math.abs(people.row - stair.row) + Math.abs(people.col - stair.col);
+			// 계단을 내려가기 시작하는 시간
 			people.possibleGoingDownStairTime = people.arrivedTime + 1;
 		}
 	}
 	
+	// 사람들이 남아있는 가?
 	public static boolean isRemainPeople() {
 		
 		for(People people : peopleList) {
+			// 사람의 도착정보가 true가 아니면 아직 안 내려간 사람이 있다는 뜻이므로 true 리턴
 			if(!people.isArrived) {
 				return true;
 			}
@@ -109,8 +131,10 @@ public class Solution {
 		return false;
 	}
 	
+	// 내려가는 사람 추가
 	static final int MAX_PEOPLE_ON_STAIR = 3;
 	public static void addPossibleGoingDownPeople() {
+		// 사람 리스트에서 아직 계단을 내려가기 시작하지 않은 사람들을 골라서 처리
 		for (int peopleIndex = 0; peopleIndex < peopleList.size(); peopleIndex++) {
 			People people = peopleList.get(peopleIndex);
 			// 계단을 내려가고 있는가?
@@ -122,14 +146,18 @@ public class Solution {
 				continue;
 			}
 			
+			// 그 사람이 고른 계단
 			Stair stair = stairList.get(selectedStairList[peopleIndex]);
 			
+			// 계단에 현재 3명 이상 있지 않고 그 사람이 현재 시간 이전에 도착한 경우
 			if(stair.goingDownPeoples.size() < MAX_PEOPLE_ON_STAIR
-					&& people.possibleGoingDownStairTime <= currentTime) {
+					&& people.possibleGoingDownStairTime == currentTime) {
 				stair.goingDownPeoples.add(people);
 				people.isGoingDown = true;
 			}
+			// 도착은 했는데 계단에 사람이 꽉 찬 경우
 			else if(people.possibleGoingDownStairTime < currentTime) {
+				// ++를 해주는 것은 계단에 들어간 시간을 알기 위함
 				people.possibleGoingDownStairTime++;
 			}
 		}
