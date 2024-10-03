f42639a5ee68d873c89f5d5571520f191bd5b327
Soohong Cha <77662632+Xylitol311@users.noreply.github.com>
2024-10-04 05:09:08 +0900

    [Platinum IV] Title: 휴대폰 자판, Time: 696 ms, Memory: 194820 KB -BaekjoonHub

--- 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.java
+++ 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.java
@@ -1,3 +1,5 @@
+// 메모리: 00000 KB / 시간: 00 ms
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,67 +8,87 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/*
+트라이를 이용해 문자열을 저장하고 자식노드가 2개 이상이거나 어느 단어의 마지막 글자인데 자식노드가 또 존재하는 경우
+숫자를 카운트 한다.
+ */
 public class Main {
-	static class TrieNode{
-		Map<Character, TrieNode> childNode = new HashMap<>();
-		boolean terminal;
-		
-		public TrieNode(){
-			/* no-op */
-		}
-		
-		public void insert(String word) {
-			TrieNode trieNode = this;
-			for(int i=0; i<word.length(); i++) {
-				char c = word.charAt(i);
-				
-				trieNode.childNode.putIfAbsent(c, new TrieNode());
-				trieNode = trieNode.childNode.get(c);
-				
-				if(i==word.length()-1) {
-					trieNode.terminal = true;
-				}
-			}
-		}
-		
-		public int autoModule(String word) { 
-			TrieNode trieNode = this;
-			int cnt=0;
-			for(int i=0; i<word.length(); i++) {
-				char c= word.charAt(i);
-				TrieNode node = trieNode.childNode.get(c);
-				if(i==0) {
-					cnt++;
-				}
-				else if(trieNode.terminal || trieNode.childNode.size()>1) {
-					cnt++;
-				}
-				trieNode = node;
-			}
-			return cnt;
-		}
-	}
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-		String line;
-		while((line = br.readLine())!=null) {
-			try {
-				int n = Integer.parseInt(line);
-				List<String> inputData = new ArrayList<>();
-				TrieNode trie = new TrieNode();
-				for(int i=0; i<n; i++) {
-					String str = br.readLine();
-					inputData.add(str);
-					trie.insert(str);
-				}
-				double res=0;
-				for(String str : inputData) {
-					res+= trie.autoModule(str);	
-				}
-				System.out.println(String.format("%.2f",res/inputData.size()));
-			}catch(NumberFormatException e) {
-				return;
-			}
-		}
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static List<String> words;
+    static TrieNode trie;
+    private static void input(String line) throws IOException {
+        // input
+        int n = Integer.parseInt(line);
+        words = new ArrayList<>();
+        trie = new TrieNode();
+        for (int i = 0; i < n; i++) {
+            String inputWord = br.readLine();
+            words.add(inputWord);
+            trie.insert(inputWord);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        String line = br.readLine();
+
+        while (line != null && !line.equals("")) {
+            input(line);
+
+            double result = 0;
+            for (String word : words) {
+                result += trie.autoModule(word);
+            }
+            double avg = result / words.size();
+            sb.append(String.format("%.2f", avg)).append("\n");
+
+            line = br.readLine();
+        }
+        System.out.println(sb);
+    }
+
+    static class TrieNode {
+        Map<Character, TrieNode> childNode = new HashMap<>();
+        boolean end;
+        int childCount = 0;
+
+        public TrieNode() {
+
+        }
+
+        public void insert(String word) {
+            TrieNode trieNode = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+
+                // 자식 노드가 존재하지 않을 경우 새로 생성
+                if (!trieNode.childNode.containsKey(c)) {
+                    trieNode.childNode.put(c, new TrieNode());
+                    trieNode.childCount++; // 자식 노드 수 증가
+                }
+                trieNode = trieNode.childNode.get(c);
+
+                // i가 단어의 마지막이라면 end를 true로 변경
+                if (i == word.length() - 1) {
+                    trieNode.end = true;
+                }
+            }
+        }
+
+        public int autoModule(String word) {
+            TrieNode trieNode = this;
+            int cnt = 0;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+
+                // 첫 글자는 항상 입력. 현재 문자의 자식 노드가 둘 이상이거나 중간 문자인데 end가 true인 경우
+                if (i == 0 || trieNode.end || trieNode.childCount > 1) {
+                    cnt++;
+                }
+
+                trieNode = trieNode.childNode.get(c);
+            }
+            return cnt;
+        }
+    }
 }
