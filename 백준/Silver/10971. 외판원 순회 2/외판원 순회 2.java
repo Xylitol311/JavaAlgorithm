ba7da199715ddecd745a143d608c2cab09fd001d
Suhong Cha <77662632+Xylitol311@users.noreply.github.com>
2026-04-13 19:15:31 +0900

    [Silver II] Title: 외판원 순회 2, Time: 64 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,46 +9,64 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    static int N, result;
+    static int N, result, fullMask;
     static int[][] map;
+    static int[][] dp;
 
     private static void input() throws IOException {
         N = Integer.parseInt(br.readLine());
         result = Integer.MAX_VALUE;
         map = new int[N][N];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+        fullMask = (1 << N) - 1;
+        dp = new int[fullMask + 1][N];
+
+        for (int[] arr : dp) {
+            Arrays.fill(arr, Integer.MAX_VALUE);
+        }
     }
 
     public static void main(String[] args) throws Exception {
         input();
-        dfs(0, 0, 0, 1);
-        System.out.println(result);
-    }
+        // 출발 지점 초기화
+        dp[1][0] = 0;
 
-    private static void dfs(int startCity, int nowCity, int sum, int isVisit) {
-        // 현재까지의 누적 비용이 이전 최저 비용보다 클 경우 취소(더 볼 필요 없음)
-        if (sum > result) return;
+        // dp 계산
+        for (int visited = 0; visited < (1 << N); visited++) {
+            // 현재도시
+            for (int city = 0; city < N; city++) {
+                // 방문한 적도 없는데 현재 도시에 있는 게 말이 안됨
+                if ((visited & 1 << city) == 0) continue;
+                // 0부터 차근차근 방문여부를 기준으로 체크하는데 INF인 경우는 이전에 방문을 못했다는 얘기이므로 통과.
+                if (dp[visited][city] == Integer.MAX_VALUE) continue;
 
-        // 전체 다 방문?
-        if (isVisit == (1 << N) - 1) {
-            // 돌아갈 수 있다면
-            if (map[nowCity][startCity] != 0) {
-                sum += map[nowCity][startCity];
-                result = Math.min(result, sum);
+                // 다음 도시
+                for (int next = 0; next < N; next++) {
+                    // 이미 방문했거나, 현재 도시 == 다음 도시이거나, 각 도시 간 연결이 없는 경우 통과
+                    if ((visited & (1 << next)) != 0 || city == next || map[city][next] == 0) continue;
+
+                    // 갈 수 있는 경우 최솟값 갱신
+                    dp[visited | 1 << next][next] = Math.min(dp[visited | 1 << next][next],
+                        dp[visited][city] + map[city][next]);
+                }
+
             }
-            return;
         }
 
-        // 다음 도시 방문
+        // 마지막 최솟값 갱신
         for (int i = 0; i < N; i++) {
-            // 아직 방문하지 않은 곳만
-            if ((isVisit & 1 << i) != 0 || map[nowCity][i] == 0) continue;
-            dfs(startCity, i, sum + map[nowCity][i], isVisit | 1 << i);
+            if (dp[fullMask][i] != Integer.MAX_VALUE && map[i][0] != 0) {
+                result = Math.min(result, dp[fullMask][i] + map[i][0]);
+            }
         }
+
+        System.out.println(result);
     }
 }
